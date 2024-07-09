1074815241886584515e8bdaf8a06c17d3ef0bbf
Jacob Wennebro <jacob.wennebro@whitelabel-loyalty.com>
2024-07-09 11:17:23 -0700

    🔧 Corrected package.json

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import JsonPathViewer from '../dist/jpv';
+import JsonPathViewer from '../dist';
 import '../dist/style.css';
 
 const json = {
