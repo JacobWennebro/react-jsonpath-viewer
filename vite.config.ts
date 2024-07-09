1e032b94e33d1e77632a49bfe36dd01d79ea9b12
Jacob Wennebro <jacob.wennebro@whitelabel-loyalty.com>
2024-07-09 11:03:56 -0700

    🔧 Corrected paths in tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       formats: ['es'],
       fileName: 'jpv',
     },
+    copyPublicDir: false,
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
